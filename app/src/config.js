a41c46916542299a77193776480f992070c404f6
Richard Kersey <github@razorit.com>
2025-03-02 21:09:59 -0500

    Add Env to Args for Vite

--- app/src/config.js
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
